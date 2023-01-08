ae3804b3f3144439eaae971759da30275614c048
Renanbot <renanbot.souza@gmail.com>
2023-01-08 16:20:43 -0300

    Duas classes feitas

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+
+        String primeiroNome = "Renan";
+        String segundoNome = "Botelho";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.print(nomeCompleto);
+        
+
+    }
+    public static String nomeCompleto( String primeiroNome, String segundoNome){
+        return " Resuldado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
