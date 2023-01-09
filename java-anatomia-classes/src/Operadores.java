380f66cdad7d4c4d657806c8216bf9bb23191090
Renanbot <renanbot.souza@gmail.com>
2023-01-09 13:22:10 -0300

    inclusão dos operadores

---
+++ java-anatomia-classes/src/Operadores.java
@@ -0,0 +1,24 @@
+import javax.sound.midi.Synthesizer;
+
+public class Operadores {
+    public static void main(String[] args) {
+        /*String nomeCompleto = "LINGUAGEM" + "Java";
+        System.out.println(nomeCompleto);
+
+        boolean variavel = true;
+        variavel = !variavel;
+
+        System.out.println(variavel);*/
+
+        //Operador Ternário
+
+        int a , b;
+        a = 5; 
+        b = 6;
+
+        String resultado = a==b ?"Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+    }
+    
+}
--- java-anatomia-classes/src/renan/tipo-variaveis/TiposDeVariaveis.java
+++ java-anatomia-classes/src/renan/tipo-variaveis/TiposDeVariaveis.java
@@ -1,9 +1,9 @@
 public class TiposDeVariaveis{
     public static void main(String[] args) {
-        double salarioMinimo = 2500;
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+        //double salarioMinimo = 2500;
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = (short) numeroNormal;
 
         int numero = 5;
         
