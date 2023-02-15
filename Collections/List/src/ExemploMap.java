675c6bb5947af771e782962d5782fa406bded765
Renanbot <renanbot.souza@gmail.com>
2023-02-14 22:01:31 -0300

    usando linkedHashMa e TreeMap

--- Collections/List/src/ExemploMap.java
+++ Collections/List/src/ExemploMap.java
@@ -1,10 +1,11 @@
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
-
-import javax.swing.GroupLayout;
+import java.util.TreeMap;
 
 public class ExemploMap {
     public static void main(String[] args) {
@@ -64,14 +65,65 @@ public class ExemploMap {
                 modelosMaisEficiente = entry.getKey();     
                 System.out.println("Modelo mais eficiente " + modelosMaisEficiente + " - " + consumoMaisEficiente);
             }
+        }   
+
+        System.out.println("Exiba o modelo menos econômico e seu consumo: ");
+
+        Double consumoMenosEficiente = Collections.min(carrosPopulares.values());
+        String modeloMenosEficiente = "";
+        for (Map.Entry<String, Double> entry : carrosPopulares.entrySet()) {
+            if(entry.getValue().equals(consumoMenosEficiente)) {
+                modeloMenosEficiente = entry.getKey();
+                System.out.println("Modelo menos eficiente: " + modeloMenosEficiente + " - " + consumoMenosEficiente);
+            }
+        } 
+
+        Iterator<Double> iterator = carrosPopulares.values().iterator();
+        Double soma = 0d;
+        
+        while(iterator.hasNext()){
+            soma += iterator.next();
+        }
+        
+        System.out.println("Exiba a soma dos consumos: " + soma);
+
+        System.out.println("Exiba a media dos consumos: " + (soma/carrosPopulares.size()));
+
+        System.out.println("Remova os modelos com o consumo igual a 15,6km/l ");
+
+        Iterator<Double> iterator1 = carrosPopulares.values().iterator();
+        
+        while(iterator1.hasNext()){
+            if(iterator1.next().equals(15.6)) iterator1.remove(); 
         }
 
+        System.out.println(carrosPopulares);
+
+        System.out.println("Exiba todos os modelos na ordem em que foram informados: ");
+        Map<String, Double> carrosPopulares1 = new LinkedHashMap<>(){{
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mobi", 16.1);
+            put("hb20", 14.5);
+            put("kwid", 15.6);
+            
+        }};
+
+        System.out.println(carrosPopulares1.toString());
+
+        System.out.println("Exiba o dicionario ordenado pelo modelo: ");
+        Map<String, Double> carrosPopulares2 = new TreeMap<>(carrosPopulares1);
+        System.out.println(carrosPopulares2.toString());
+
+        System.out.println("Apague o conjunto criado.");
+        carrosPopulares.clear();
+
+        System.out.println("Confira de o conjunto está vazio: " + carrosPopulares.isEmpty());
 
 
-    
-    
-    
-    
-        }
+
+
+    }
+
     
 }
